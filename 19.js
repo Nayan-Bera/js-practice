b2b5fd9f6b28f8cd4e3d1af2611e4ee2a461db42
Nayan Bera <nayanbera9@gmail.com>
2024-08-14 23:40:14 +0530

    add new file

---
+++ 19.js
@@ -0,0 +1,11 @@
+console.log("Display the current date and time")
+
+function getcurrrentdateandtime(){
+    let currentDate= new Date();
+return currentDate.toLocaleString()
+
+}
+console.log(getcurrrentdateandtime())
+
+
+//console.log(new Date())
